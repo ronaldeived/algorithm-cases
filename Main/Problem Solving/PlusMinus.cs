1c167902e33d5fc13d3f687c7c336378de01038c
Ronaldeived Silva <rona_dev@outlook.com>
2023-04-26 22:35:12 -0300

    Plus minus test

---
+++ Main/Problem Solving/PlusMinus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Main.ProblemSolving
+{
+    public abstract class PlusMinus
+    {
+        public static void PlusMinusFunction(List<int> arr)
+        {
+            if (!arr.Any())
+            {
+                Console.Write("Empty List");
+                return;
+            }
+
+            var total = arr.Count();
+            var positive = arr.Count(x => x > 0);
+            var negative = arr.Count(x => x < 0);
+            var zero = arr.Count(x => x == 0);
+
+            decimal positiveResult = decimal.Divide(positive, total);
+            decimal negativeResult = decimal.Divide(negative, total);
+            decimal zeroResult = decimal.Divide(zero, total);
+
+            Console.WriteLine(positiveResult.ToString("N6"));
+            Console.WriteLine(negativeResult.ToString("N6"));
+            Console.WriteLine(zeroResult.ToString("N6"));
+        }
+    }
+}
+
---
+++ MainTests/Problem Solving/PlusMinusSpec.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Main.ProblemSolving;
+
+namespace MainTests.ProblemSolving
+{
+    public class PlusMinusSpec
+    {
+        [Fact]
+        public void ShouldPrintSixScaleValues()
+        {
+            var consoleValue = new StringWriter();
+
+            Console.SetOut(consoleValue);
+
+            PlusMinus.PlusMinusFunction(new List<int> { 1, 2, 3, -1, -2, -3, 0, 0 });
+
+            var consoleOutputList = consoleValue.ToString().Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            foreach (var item in consoleOutputList)
+            {
+                int index = item.IndexOf(',');
+
+                string restAfterComma = item.Substring(index + 1);
+
+                int legth = restAfterComma.Length;
+
+                legth.Should().Be(6);
+            }
+
+        }
+
+        [Fact]
+        public void ShouldGetCorretlyValues()
+        {
+            var consoleValue = new StringWriter();
+
+            Console.SetOut(consoleValue);
+
+            PlusMinus.PlusMinusFunction(new List<int> { -4, 3, -9, 0, 4, 1 });
+
+            var consoleOutputList = consoleValue.ToString().Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            decimal.TryParse(consoleOutputList[0], out decimal result1);
+            decimal.TryParse(consoleOutputList[1], out decimal result2);
+            decimal.TryParse(consoleOutputList[2], out decimal result3);
+
+            result1.Should().Be(0.500000m);
+            result2.Should().Be(0.333333m);
+            result3.Should().Be(0.166667m);
+        }
+
+        [Fact]
+        public void ShouldGetMessagePassingEmptyList()
+        {
+            var consoleValue = new StringWriter();
+
+            Console.SetOut(consoleValue);
+
+            PlusMinus.PlusMinusFunction(new List<int>());
+
+            consoleValue.ToString().Should().Be("Empty List");
+        }
+    }
+}
+
