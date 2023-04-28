f162d0442fac29d965018e9f7f1bb9751f733962
Ronaldeived Silva <rona_dev@outlook.com>
2023-04-28 00:01:34 -0300

    Min max sum challenge with unit tests

---
+++ Main/Problem Solving/Min Max Sum/MinMaxSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Main.ProblemSolving.MinMaxSum
+{
+    public static class MinMaxSum
+    {
+        public static void MiniMaxSumFunction(List<int> arr)
+        {
+            var minIndex = arr.IndexOf(arr.Min());
+            var maxIndex = arr.IndexOf(arr.Max());
+
+            var minimum = arr
+                .Select((item, index) => index != maxIndex ? (ulong)item : 0)
+                .ToList()
+                .Aggregate((a, b) => a + b);
+
+            var maximum = arr
+                .Select((item, index) => index != minIndex ? (ulong)item : 0)
+                .ToList()
+                .Aggregate((a,b) => a + b);
+
+            Console.Write($"{minimum} {maximum}");
+        }
+    }
+}
+
---
+++ MainTests/Problem Solving/MinMaxSumSpec.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Main.ProblemSolving;
+using Main.ProblemSolving.MinMaxSum;
+
+namespace MainTests.ProblemSolving
+{
+    public class MinMaxSumSpec
+    {
+        [Theory]
+        [MemberData(nameof(MinMaxSumParameters))]
+        private void ShouldSumCorretly(Model model, Result result)
+        {
+            var consoleValue = new StringWriter();
+            Console.Clear();
+
+            Console.SetOut(consoleValue);
+
+            MinMaxSum.MiniMaxSumFunction(model.Content ?? new List<int>());
+
+            var consoleOutput = consoleValue.ToString();
+
+            Assert.Equivalent(result.ExpectedResult, consoleOutput);
+            Console.Clear();
+        }
+
+        public static IEnumerable<object[]> MinMaxSumParameters()
+        {
+            yield return new object[]
+            {
+                new Model
+                {
+                    Content = new List<int>{ 256741038, 623958417, 467905213, 714532089, 938071625 }
+                },
+                new Result
+                {
+                    ExpectedResult = "2063136757 2744467344"
+                }
+            };
+
+            yield return new object[]
+            {
+                new Model
+                {
+                    Content = new List<int>{ 1, 3, 5, 7, 9 }
+                },
+                new Result
+                {
+                    ExpectedResult = "16 24"
+                }
+            };
+
+            yield return new object[]
+            {
+                new Model
+                {
+                    Content = new List<int>{ 5, 5, 5, 5, 5 }
+                },
+                new Result
+                {
+                    ExpectedResult = "20 20"
+                }
+            };
+        }
+    }
+
+    public class Model
+    {
+        public List<int>? Content { get; set; }
+    }
+
+    public class Result
+    {
+        public string? ExpectedResult { get; set; }
+    }
+}
+
