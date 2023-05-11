0d54d4de15a2d67753c2bff4eaed4653590d86fc
Ronaldeived Silva <rona_dev@outlook.com>
2023-05-11 15:22:48 -0300

    Challenger to verify Multiple and unit tests

---
+++ Main/Problem Solving/Multiple/Multiple.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace Main.ProblemSolving.Multiple
+{
+    public static class Multiple
+    {
+        public static string FizzBuzz(int n)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result.AppendLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.AppendLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.AppendLine("Buzz");
+                }
+                else
+                {
+                    result.AppendLine(i.ToString());
+                }
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
+
---
+++ MainTests/Problem Solving/MultipleSpec.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Main.ProblemSolving.Multiple;
+using Main.ProblemSolving.TimeConversion;
+
+namespace MainTests.ProblemSolving
+{
+    public class MultipleSpec
+    {
+        [Theory]
+        [MemberData(nameof(MultipleParameters))]
+        private void ShouldExpectMultipleCorretly(dynamic model, dynamic expected)
+        {
+            var result = Multiple.FizzBuzz(model.Content);
+
+            Assert.Equivalent(result, expected.ExpectedResult);
+        }
+
+        public static IEnumerable<object[]> MultipleParameters()
+        {
+            yield return new object[]
+            {
+                new
+                {
+                    Content = 15
+                },
+                new
+                {
+                    ExpectedResult = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz".TrimEnd()
+                }
+            };
+
+            yield return new object[]
+            {
+                new
+                {
+                    Content = 65
+                },
+                new
+                {
+                    ExpectedResult = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\nBuzz\nFizz\n37\n38\nFizz\nBuzz\n41\nFizz\n43\n44\nFizzBuzz\n46\n47\nFizz\n49\nBuzz\nFizz\n52\n53\nFizz\nBuzz\n56\nFizz\n58\n59\nFizzBuzz\n61\n62\nFizz\n64\nBuzz".TrimEnd()
+                }
+            };
+
+            yield return new object[]
+            {
+                new
+                {
+                    Content = 12
+                },
+                new
+                {
+                    ExpectedResult = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz".TrimEnd()
+                }
+            };
+        }
+    }
+}
+
