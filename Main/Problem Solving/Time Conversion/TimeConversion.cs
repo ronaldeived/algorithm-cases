4b6fec882304ccbd657bb0c6b812431bf5cd938c
Ronaldeived Silva <rona_dev@outlook.com>
2023-05-11 14:01:21 -0300

    Challenger to convert time and unit tests

---
+++ Main/Problem Solving/Time Conversion/TimeConversion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Main.ProblemSolving.TimeConversion
+{
+    public static class TimeConversion
+    {
+        public static string TimeConversionFunction(string s)
+        {
+            DateTime dateTime;
+
+            if (!DateTime.TryParse(s, out dateTime))
+            {
+                throw new ArgumentException("Input must be a valid time string in the format hh:mm:ssAM or hh:mm:ssPM.", nameof(s));
+            }
+
+            return dateTime.ToString("HH:mm:ss");
+        }
+    }
+}
+
---
+++ MainTests/Problem Solving/TimeConversionSpec.cs
@@ -0,0 +1,56 @@
+﻿using Main.ProblemSolving.TimeConversion;
+
+namespace MainTests.ProblemSolving
+{
+    public class TimeConversionSpec
+    {
+        [Theory]
+        [MemberData(nameof(TimeConversionParameters))]
+        private void ShouldConvertTime(dynamic model, dynamic expected)
+        {
+            var result = TimeConversion.TimeConversionFunction(model.Content ?? string.Empty);
+
+            Assert.Equivalent(result, expected.ExpectedResult);
+        }
+
+        public static IEnumerable<object[]> TimeConversionParameters()
+        {
+            yield return new object[]
+            {
+                new
+                {
+                    Content = "07:05:45PM"
+                },
+                new
+                {
+                    ExpectedResult = "19:05:45"
+                }
+            };
+
+            yield return new object[]
+            {
+                new
+                {
+                    Content = "12:20:10PM"
+                },
+                new
+                {
+                    ExpectedResult = "12:20:10"
+                }
+            };
+
+            yield return new object[]
+            {
+                new
+                {
+                    Content = "12:45:22AM"
+                },
+                new
+                {
+                    ExpectedResult = "00:45:22"
+                }
+            };
+        }
+    }
+}
+
