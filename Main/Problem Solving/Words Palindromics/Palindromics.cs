bccdc4a08fca8e49e37452bb76ce8bcfc0cc8b57
Ronaldeived Silva <rona_dev@outlook.com>
2023-11-07 07:12:42 -0300

    adding tests to palindromics words

---
+++ Main/Problem Solving/Words Palindromics/Palindromics.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Main.ProblemSolving.WordsPalindromics
+{
+    public static class Palindromics
+    {
+        public static string GetPalindromics(string word)
+        {
+            string? result = LongestPalindrome(word);
+
+            if (result != null && result.Length > 2)
+            {
+                return result;
+            }
+            else
+            {
+                return "none";
+            }
+        }
+
+        private static string? LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return null;
+
+            string longest = s.Substring(0, 1);
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                // Teste com i como ponto central
+                string temp = ExpandAroundCenter(s, i, i);
+                if (temp.Length > longest.Length)
+                {
+                    longest = temp;
+                }
+
+                // Teste com i e i+1 como pontos centrais
+                temp = ExpandAroundCenter(s, i, i + 1);
+                if (temp.Length > longest.Length)
+                {
+                    longest = temp;
+                }
+            }
+            return longest;
+        }
+
+        private static string ExpandAroundCenter(string s, int left, int right)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+            return s.Substring(left + 1, right - left - 1);
+        }
+    }
+}
+
---
+++ MainTests/Problem Solving/PalindromicsSpec.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Main.ProblemSolving.WordsPalindromics;
+
+namespace MainTests.ProblemSolving
+{
+    public class PalindromicsSpec
+    {
+        [Theory]
+        [MemberData(nameof(TestPalindromics))]
+        private void ShouldExpectMultipleCorretly(dynamic model, dynamic expected)
+        {
+            var result = Palindromics.GetPalindromics(model.Content);
+
+            Assert.Equivalent(result, expected.ExpectedResult);
+        }
+
+        public static IEnumerable<object[]> TestPalindromics()
+        {
+            yield return new object[]
+            {
+                new
+                {
+                    Content = "abssolosl"
+                },
+                new
+                {
+                    ExpectedResult = "solos"
+                }
+            };
+
+            yield return new object[]
+            {
+                new
+                {
+                    Content = "abcc"
+                },
+                new
+                {
+                    ExpectedResult = "none"
+                }
+            };
+
+            yield return new object[]
+            {
+                new
+                {
+                    Content = "abccc"
+                },
+                new
+                {
+                    ExpectedResult = "ccc"
+                }
+            };
+
+            yield return new object[]
+            {
+                new
+                {
+                    Content = "kjrotatorj"
+                },
+                new
+                {
+                    ExpectedResult = "jrotatorj"
+                }
+            };
+        }
+    }
+}
+
